a1bb5cb55ab1a3889425962b871ca4981f5940ad
nizarakbarmei <nizar.akbar.m@mail.ugm.ac.id>
2016-09-13 09:12:19 +0700

    second commit

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def dec(e):
 	decstr=base64.b64decode(e)
 	print "Decoding %s......." %e
 	return xmlrpclib.Binary(decstr)
-server=SimpleXMLRPCServer(("localhost",8000))
+server=SimpleXMLRPCServer(("192.168.1.17",8000))
 print "Starting xml server on port 8000...."
 server.register_function(dec,'dec')
 
